7759fb90e54ce2cca6f0efb5716dfa9606798fed
Profesor C1 <admin@PROF-C1.local>
2025-05-21 19:17:15 +0300

    curs3

---
+++ curs2/comenzi.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `magazin`.`produse` (`idprodus` INT UNSIGNED NOT NULL AUTO_INCREMENT , `numeprodus` VARCHAR(100) NOT NULL , `cantitate` INT NOT NULL DEFAULT '0' , `idfirma` INT NOT NULL , `firma` VARCHAR(50) NOT NULL , `adresafirma` VARCHAR(100) NULL , `modelprodus` VARCHAR(50) NOT NULL , `stocprodus` INT NOT NULL DEFAULT '0' , `pret` DECIMAL(10,4) NOT NULL , `cagtegoriep` VARCHAR(100) NOT NULL , `descrierp` TEXT NOT NULL , `datadaugare` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`idprodus`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
+
+
+CREATE TABLE `magazin`.`firme` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT , `nume` VARCHAR(100) NOT NULL , `adresa` VARCHAR(200) NOT NULL , `dataadaugare` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;
